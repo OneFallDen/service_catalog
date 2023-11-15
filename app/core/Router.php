5f7393e81dc0e08a454268aa55d64bad10408190
OneFallDen <onefallden@gmail.com>
2023-11-15 19:28:28 +0300

    router class

---
+++ app/core/Router.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace app\core;
+
+class Router {
+
+    protected $routes = [];
+    protected $params = [];
+
+    public function __construct() {
+        $arr = require 'app/config/routes.php';
+        foreach ($arr as $key => $val){
+            $this->add($key, $val);
+        }
+    }
+
+    public function add($route, $params) {
+        $route = '#^'.$route.'$#';
+        $this->routes[$route] = $params;
+    }
+
+    public function match() {
+
+        $url = trim($_SERVER['REQUEST_URI'],'/');
+        $urls = explode('/', $url);
+        $url = '';
+        for ($i = 1; $i < count($urls); $i++) {
+            $url = $url.$urls[$i].'/';
+        }
+        $url = trim($url, '/');
+
+        foreach ($this->routes as $route => $params){
+            if(preg_match($route, $url, $matches)){
+                $this->params = $params;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function run() {
+        if($this->match()){
+            $path = 'app\contollers\\'.ucfirst($this->params['controller']).'Controller';
+            if (class_exists($path)){
+                $action = $this->params['action'].'Action';
+                if (method_exists($path, $action)){
+                    $controller = new $path;
+                    $controller->$action();
+                } else {
+                    echo 'Page not found :('; // Add 404 page
+                }
+            } else {
+                echo 'Page not found :('; // Add 404 page
+            }
+        } else {
+            echo 'Page not found :('; // Add 404 page
+        }
+    }
+}
