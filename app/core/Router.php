eb10c3d94e812e0834a8adf9295953e425d09226
OneFallDen <onefallden@gmail.com>
2023-11-15 20:30:00 +0300

    added redirect to 404 page

--- app/core/Router.php
+++ app/core/Router.php
@@ -1,60 +1,62 @@
 <?php
 
-namespace app\core;
+    namespace app\core;
 
-class Router {
+    use app\core\View;
 
-    protected $routes = [];
-    protected $params = [];
+    class Router {
 
-    public function __construct() {
-        $arr = require 'app/config/routes.php';
-        foreach ($arr as $key => $val){
-            $this->add($key, $val);
-        }
-    }
+        protected $routes = [];
+        protected $params = [];
 
-    public function add($route, $params) {
-        $route = '#^'.$route.'$#';
-        $this->routes[$route] = $params;
-    }
-
-    public function match() {
-
-        $url = trim($_SERVER['REQUEST_URI'],'/');
-        $urls = explode('/', $url);
-        $url = '';
-        for ($i = 1; $i < count($urls); $i++) {
-            $url = $url.$urls[$i].'/';
-        }
-        $url = trim($url, '/');
-
-        foreach ($this->routes as $route => $params){
-            if(preg_match($route, $url, $matches)){
-                $this->params = $params;
-                return true;
+        public function __construct() {
+            $arr = require 'app/config/routes.php';
+            foreach ($arr as $key => $val){
+                $this->add($key, $val);
             }
         }
 
-        return false;
-    }
+        public function add($route, $params) {
+            $route = '#^'.$route.'$#';
+            $this->routes[$route] = $params;
+        }
 
-    public function run() {
-        if($this->match()){
-            $path = 'app\controllers\\'.ucfirst($this->params['controller']).'Controller';
-            if (class_exists($path)){
-                $action = $this->params['action'].'Action';
-                if (method_exists($path, $action)){
-                    $controller = new $path($this->params);
-                    $controller->$action();
+        public function match() {
+
+            $url = trim($_SERVER['REQUEST_URI'],'/');
+            $urls = explode('/', $url);
+            $url = '';
+            for ($i = 1; $i < count($urls); $i++) {
+                $url = $url.$urls[$i].'/';
+            }
+            $url = trim($url, '/');
+
+            foreach ($this->routes as $route => $params){
+                if(preg_match($route, $url, $matches)){
+                    $this->params = $params;
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public function run() {
+            if($this->match()){
+                $path = 'app\controllers\\'.ucfirst($this->params['controller']).'Controller';
+                if (class_exists($path)){
+                    $action = $this->params['action'].'Action';
+                    if (method_exists($path, $action)){
+                        $controller = new $path($this->params);
+                        $controller->$action();
+                    } else {
+                        View::errorCode(404);
+                    }
                 } else {
-                    echo 'Page not found :('; // Add 404 page
+                    View::errorCode(404);
                 }
             } else {
-                echo 'Page not found :('; // Add 404 page
+                View::errorCode(404);
             }
-        } else {
-            echo 'Page not found :('; // Add 404 page
         }
     }
-}
