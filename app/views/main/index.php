22accc2bad0f39a88330457d8f08b232af47eeb1
OneFallDen <onefallden@gmail.com>
2023-12-15 12:41:08 +0300

    added main page view

--- app/views/main/index.php
+++ app/views/main/index.php
@@ -1,1 +1,15 @@
-<p>Main page</p>
+<div class="hello">
+<p class="hello__text">Добро пожаловать!</p>
+<p>С чем связана ваша проблема?</p>
+</div>
+
+<ul class="block__list">
+    <?php while ($val = $result->GetNext()): ?>
+        <li class="block__item">
+            <?php $href="parent/select?mainSectionName=".$val["NAME"]."&parentSectionId=".$val["ID"]?>
+            <a class="chapter" href="<?=$href?>">
+                <p><?=$val["NAME"]?></p>
+            </a>
+        </li>
+    <?php endwhile; ?>
+</ul>
